1a423dbcedaa82bdbee9cf7e39965567daeddd6c
ccnextgen <103283908+ccnextgen@users.noreply.github.com>
2022-04-09 01:31:17 +0530

    port change

--- src/openapi-spec.ts
+++ src/openapi-spec.ts
@@ -7,7 +7,7 @@ import {OnlinePaperLoopbackApplication} from './application';
 async function exportOpenApiSpec(): Promise<void> {
   const config: ApplicationConfig = {
     rest: {
-      port: +(process.env.PORT ?? 3000),
+      port: +(process.env.PORT ?? 8080),
       host: process.env.HOST ?? 'localhost',
     },
   };
